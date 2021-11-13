b95640704c032fd44a253a936dad77af089942a1
Jerome Horton <jeromehorton1@gmail.com>
2021-11-13 17:04:02 -0600

    added calculation conditional function

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,27 @@
-$(document).ready(onReady);
+$(document).ready(goTime);
+
+function goTime(){
+   console.log("Let's Roll!"); 
+   $('#equal-btn').on('click', calcNumbers);
+}
+
+
+function calcNumbers(){
+    let numbersObject = {
+        num1: $('#first-number').val(),
+        num2: $('#second-number').val(),
+        operator: operator
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/calculate',
+        data: numbersObject,
+    }).then((response) => {
+        console.log(response);
+      }).catch((error) => {
+        console.log('calculation failed', error)
+      });
+}
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,15 +1,51 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+let calculationArray=[];
+
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+app.get('calculate', (req,res) => {
+    res.send('hey express');
+    solvingArray();
+    res.send(solvingArray);
+});
+
+app.post('/calculate', (req,res) => {
+    console.log("show calculation:", req.body);
+    calculationArray.push(req.body)
+    console.log(calculationArray);
+    res.send(201);
+  });
+
+// create a function for if's conditional
+
+function solvingArray(){
+    for (let solving of calculationArray){
+      if(solving.operator  === '+'){
+        solving.answer = Number(items.numOne) + Number(items.numTwo);
+      }else if (solving.operator  === '-'){
+        solving.answer = Number(items.numOne) - Number(items.numTwo);
+      }else if (solving.operator === '*'){
+        solving.answer = Number(items.numOne) * Number(items.numTwo);
+      }else if (solving.operator === '/'){
+        solving.answer = Number(items.numOne) / Number(items.numTwo);
+    };
+   }
+    console.log(calculationArray);
+   console.log(calculationProblem);
+   };
 
 
 
 
-
-app.listen(PORT, () => {
+app.listen(5000, (res) => {
     console.log('listening on port', PORT)
 });
