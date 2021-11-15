79f24aba911ca3b67aa29a19af123c51e119e6ee
Jerome Horton <jeromehorton1@gmail.com>
2021-11-14 19:33:21 -0600

    fixed History Route error

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const PORT = 5000;
 let calculationArray=[];
 let historyArray = [];
 
+let sum = 0;
+
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -17,13 +19,14 @@ app.use(express.static('server/public'));
 
 app.post('/values', (req, res) =>{
       console.log('req is', req.body);
-      calculationArray.push(req.body);
-      solvingMath();
+      // calculationArray.push(req.body);
+      // historyArray = [];
+      solvingMath(req.body);
       console.log(calculationArray);
       res.sendStatus(201);
 });
 app.get('/history', (req, res) => {
-    console.log('history path', req.body.path);
+    console.log('history path', req.route.path);
     // solvingMath();
     res.send(historyArray);
  });
@@ -37,35 +40,37 @@ app.get('/history', (req, res) => {
 
 // create a function for if's conditional
 
-  function solvingMath(){ 
-      for (let aNumber of calculationArray){
+  function solvingMath(aNumber){ 
+      // for (let aNumber of calculationArray){
+     
         let fullSolve = {
+          // sum: sum,
           num1: aNumber.num1,
           num2: aNumber.num2,
           numBtn: aNumber.button
         }
       if (aNumber.button === "add-btn") {
-          fullSolve.sum = Number(aNumber.num1) + Number(aNumber.num2);
+         fullSolve.sum = Number(aNumber.num1) + Number(aNumber.num2);
           calculationArray.push(fullSolve.sum);
 
     }
       else if (aNumber.button === "minus-btn") {
-        fullSolve.sum = Number(aNumber.num1) - Number(aNumber.num2);
+       fullSolve.sum = Number(aNumber.num1) - Number(aNumber.num2);
           calculationArray.push(fullSolve.sum);
     }
       else if (aNumber.button === "multiply-btn") {
-        fullSolve.sum = Number(aNumber.num1) * Number(aNumber.num2);
+       fullSolve.sum = Number(aNumber.num1) * Number(aNumber.num2);
           calculationArray.push(fullSolve.sum);
     }
       else if (aNumber.button === "divide-btn") {
-        fullSolve.sum = Number(aNumber.num1) / Number(aNumber.num2);
+       fullSolve.sum = Number(aNumber.num1) / Number(aNumber.num2);
           calculationArray.push(fullSolve.sum);
     }
       historyArray.push(fullSolve);
   }
       console.log(historyArray);
       console.log(calculationArray);
-}
+// }
 
     app.listen(PORT, (res) => {
       console.log('listening on port', PORT)
